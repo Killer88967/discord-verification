2ee7fb6a55b26f2372e53229a9851201600c349f
Killer88967 <Erniebug27@gmail.com>
2026-09-10 19:38:26 +0000

    Changed database exports

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,33 +1,8 @@
-export { prisma } from "./prisma.js";
-
-export {
-  completeVerificationSession,
-  createVerificationSession,
-  getVerificationSessionByToken,
-  type CompleteVerificationSessionResult,
-  type CreateVerificationSessionOptions,
-  type CreatedVerificationSession,
-  type VerificationSessionLookupResult,
-} from "./verificationSession.js";
-
-export {
-  getRoleAssignmentTarget,
-  type RoleAssignmentTargetResult,
-} from "./roleAssignment.js";
-
-export { configureGuild, type ConfigureGuildOptions } from "./guildConfig.js";
-
-export {
-  storeVerificationSignals,
-  type StoredVerificationSignal,
-  type StoreVerificationSignalsOptions,
-} from "./verificationSignals.js";
-
-export { findDeviceMatches, type DeviceMatch } from "./deviceMatches.js";
-
-export {
-  getAccountLinksForUser,
-  upsertAccountLink,
-  type AccountLinkMatch,
-  type UpsertAccountLinkOptions,
-} from "./accountLinks.js";
+export * from "./prisma.js";
+export * from "./verificationSession.js";
+export * from "./roleAssignment.js";
+export * from "./guildConfig.js";
+export * from "./verificationSignals.js";
+export * from "./deviceMatches.js";
+export * from "./accountLinks.js";
+export * from "./signalMatches.js";
